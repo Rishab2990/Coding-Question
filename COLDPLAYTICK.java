d3b989d815de5146170bbf1dfb42f33945d4b354
Rishab Singh <101442948+Rishab2990@users.noreply.github.com>
2024-10-09 23:42:01 +0530

    Add files via upload
    Coldplay Tickets Problem
    Problem Statement: You are going to the Coldplay concert with N friends and buying tickets for everyone. Each ticket costs 5000 INR. Calculate the total amount to pay for all tickets, including your own.
    
    Input:
    
    A single integer N (number of friends).
    Output:
    
    Total cost in INR for N + 1 tickets.
    Constraints:
    
    1
    ≤
    𝑁
    ≤
    5
    1≤N≤5
    Example:
    
    Input: 1
    Output: 10000
    
    Input: 5
    Output: 30000

---
+++ COLDPLAYTICK.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void main(String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int Total_friends = N + 1;
+
+        // 		for(int i = 1 ; i<=N ; i++){
+
+        // 		System.out.println(i*5000);
+        // 		}
+
+
+        System.out.println(Total_friends * 5000);
+
+
+    }
+}
